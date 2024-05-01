231bb908321171463e3a66bc7bb0bdd335561dbf
Britainistrash <159732978+Britainistrash@users.noreply.github.com>
2024-05-02 10:29:41 +1200

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+input ("What's your name")
+
+print ("Welcome to the quiz")
+print ("This quiz is about the stuff in the universe")
+
+input ("What galaxy are we in?")
+print ("Haha noob we're in milky way")
+
+print ("Thx for playing please do not try again :)")
