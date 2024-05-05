035d124f006063c67face92cd789b0f947e97263
Britainistrash <159732978+Britainistrash@users.noreply.github.com>
2024-05-06 11:58:26 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-input ("What's your name")
+name = input ("What's your name")
+if name == "Human":
+    print("That's cool")
+print("My name is NevER gONnA giVE yOU Up")
 
 print ("Welcome to the quiz")
 print ("This quiz is about the stuff in the universe")
@@ -7,3 +10,5 @@ input ("What galaxy are we in?")
 print ("Haha noob we're in milky way")
 
 print ("Thx for playing please do not try again :)")
+
+answer = input("What galaxy are we in?")
