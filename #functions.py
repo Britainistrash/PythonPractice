fc013620f595de79ef778137c7761ed70f5afc10
Britainistrash <159732978+Britainistrash@users.noreply.github.com>
2024-05-30 11:21:23 +1200

    Update #functions.py

--- #functions.py
+++ #functions.py
@@ -1,20 +1,59 @@
+guesses = []
+BIGGEST_COUNTRIES_ANSWERS =["russia", "canada", "china", "us", "brazil", "australlia", "india", "argentina"]
+
 #functions
+
 def intro():
     #name
     name = input("What's your name?")
 
-    print("Welcome to this quiz,", name)
-    print("This quiz is about the ten largest countries in the world. Can you name them?")
+    print("Welcome to the quiz,", name)
+    print("The quiz is about the ten largest countries in the world")
 
-#main code
+#ask lives
 def getLives():
     while True:
         lives = input("How many chances do you want?")
         try:
+            #if the number is okay
             lives = int(lives)
             if lives >= 0:
                 return lives
             else:
-                print("Please chose a positive number")
+                print("chose a number")
         except:
-            print("That wasn't a number")
+            print("That wasn't a nummber")
+
+#if already exists
+def inList(answer, list):
+    if answer in list:
+        return True
+    else:
+        return False
+    
+#main code
+intro()
+lives = getLives()
+score = 0
+
+#quiz starts
+while lives > 0:
+    answer = input("Name one of the top 10 biggest countries in the world:\n").lower()
+
+    #wrong or correct answer
+    if inList(answer, BIGGEST_COUNTRIES_ANSWERS):
+        #if gueesed or not aready
+        if inList(answer, guesses):
+            print("You have already guessed that already")
+        else:
+            print("Correct")
+            score += 5
+            guesses.append(answer)
+            print("You have guessed {}. Your score is {}. You have {} chances remaining".format(len(guesses), score, lives))
+    else:
+        print("Wrong")
+        lives -= 1
+        print("You have guessed {}. Your score is {}. You have {} chances reamaing".format(len(guesses),score, lives))
+
+#quiz end
+print("GG. Your final score was {}".format(score))
